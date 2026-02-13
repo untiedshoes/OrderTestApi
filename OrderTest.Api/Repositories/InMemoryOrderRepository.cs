71aced2783e05ca2f14ef7f8101f61dd29cbc758
untiedshoes <untiedshoes@hotmail.com>
2026-02-13 14:03:27 +0000

    Refactored InMemoryOrderRepository to use a ConcurrentDictionary<int, Order>, which removes the need for explicit locks while keeping thread safety and fast ID lookup (Just for fun!).

--- OrderTest.Api/Repositories/InMemoryOrderRepository.cs
+++ OrderTest.Api/Repositories/InMemoryOrderRepository.cs
@@ -1,4 +1,5 @@
 using OrderTest.Api.Models;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 
@@ -6,40 +7,24 @@ namespace OrderTest.Api.Repositories;
 
 public class InMemoryOrderRepository : IOrderRepository
 {
-    private static List<Order> _orders = new();
-    private static readonly object _lock = new();
+    private static ConcurrentDictionary<int, Order> _orders = new();
 
     public Task AddAsync(Order order)
     {
-        //ensure some level of thread safety
-        lock (_lock)
-        {
-            _orders.Add(order);
-            return Task.FromResult(order);
-        }
+        _orders[order.Id] = order; // thread-safe without explicit lock
+        return Task.FromResult(order);
     }
 
     public Task<Order?> GetByIdAsync(int id)
     {
-        Order? result;
-        //ensure some level of thread safety
-        lock (_lock)
-        {
-            result = _orders.FirstOrDefault(o => o.Id == id);
-        }
+        _orders.TryGetValue(id, out var result);
         return Task.FromResult(result);
     }
 
     public Task<IEnumerable<Order>> GetAllAsync()
     {
-        IEnumerable<Order> orders;
-
-        //ensure some level of thread safety
-        lock (_lock)
-        {
-            orders = _orders.ToList();
-        }
-
-        return Task.FromResult((IEnumerable<Order>) orders);
+        // Return a snapshot of all orders
+        var snapshot = _orders.Values.ToList();
+        return Task.FromResult((IEnumerable<Order>)snapshot);
     }
 }
