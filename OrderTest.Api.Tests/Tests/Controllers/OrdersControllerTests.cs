9ffc2d3dbfb5e5b74c4a81bb53eec2768337813a
untiedshoes <untiedshoes@hotmail.com>
2026-02-13 16:44:14 +0000

    Split the tests into two clean classes - To mirror main folder structure.

---
+++ OrderTest.Api.Tests/Tests/Controllers/OrdersControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using OrderTest.Api.Controllers;
+using OrderTest.Api.Models;
+using OrderTest.Api.Services;
+using System.Threading.Tasks;
+
+namespace OrderTest.Api.Tests.Controllers;
+
+[TestFixture]
+public class OrdersControllerTests
+{
+    private readonly Mock<IOrderService> _orderServiceMock;
+    private readonly OrdersController _controller;
+
+    public OrdersControllerTests()
+    {
+        _orderServiceMock = new Mock<IOrderService>();
+        _controller = new OrdersController(_orderServiceMock.Object);
+    }
+
+    [Test]
+    public async Task GetAsync_ShouldReturnOk_WhenOrderExists()
+    {
+        var order = new Order { 
+            Id = 1,
+            CustomerEmail = "craig@untiedshoes.co.uk" 
+        };
+        _orderServiceMock.Setup(s => s.GetOrderAsync(1)).ReturnsAsync(order);
+
+        var result = await _controller.GetAsync(1);
+
+        Assert.That(result, Is.TypeOf<OkObjectResult>());
+        var okResult = result as OkObjectResult;
+        Assert.That(okResult!.Value, Is.EqualTo(order));
+        _orderServiceMock.Verify(s => s.GetOrderAsync(1), Times.Once);
+    }
+
+    [Test]
+    public async Task GetAsync_ShouldReturnNotFound_WhenOrderDoesNotExist()
+    {
+        _orderServiceMock.Setup(s => s.GetOrderAsync(42)).ReturnsAsync((Order?)null);
+
+        var result = await _controller.GetAsync(42);
+
+        Assert.That(result, Is.TypeOf<NotFoundResult>());
+        _orderServiceMock.Verify(s => s.GetOrderAsync(42), Times.Once);
+    }
+}
--- OrderTest.Api.Tests/Tests/Services/OrderServiceTests.cs
+++ OrderTest.Api.Tests/Tests/Services/OrderServiceTests.cs
@@ -1,43 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
-using OrderTest.Api.Controllers;
 using OrderTest.Api.Models;
 using OrderTest.Api.Repositories;
 using OrderTest.Api.Services;
 using System;
 using System.Threading.Tasks;
 
-namespace OrderTest.Api.Tests;
+namespace OrderTest.Api.Tests.Services;
 
 [TestFixture]
-public class OrderTests
+public class OrderServiceTests
 {
     private readonly Mock<IOrderRepository> _orderRepositoryMock;
     private readonly Mock<IEmailNotificationService> _emailNotificationServiceMock;
     private readonly Mock<ILogger<OrderService>> _loggerMock;
-    private readonly Mock<IOrderService> _orderServiceMock;
-
     private readonly OrderService _orderService;
-    private readonly OrdersController _ordersController;
 
-    public OrderTests()
+    public OrderServiceTests()
     {
         _orderRepositoryMock = new Mock<IOrderRepository>();
         _emailNotificationServiceMock = new Mock<IEmailNotificationService>();
         _loggerMock = new Mock<ILogger<OrderService>>();
-        _orderServiceMock = new Mock<IOrderService>();
 
         _orderService = new OrderService(
             _orderRepositoryMock.Object,
             _emailNotificationServiceMock.Object,
             _loggerMock.Object);
-
-        _ordersController = new OrdersController(_orderServiceMock.Object);
     }
-
-    // OrderService Tests
 
     [Test]
     public void CreateOrderAsync_ShouldThrowArgumentNullException_WhenOrderIsNull()
@@ -66,13 +56,11 @@ public class OrderTests
             CustomerEmail = "craig@untiedshoes.co.uk" 
         };
 
-        _orderRepositoryMock
-            .Setup(o => o.AddAsync(It.IsAny<Order>()))
-            .Returns(Task.CompletedTask);
+        _orderRepositoryMock.Setup(o => o.AddAsync(It.IsAny<Order>()))
+                            .Returns(Task.CompletedTask);
 
-        _emailNotificationServiceMock
-            .Setup(e => e.SendEmailAsync(
-                It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+        _emailNotificationServiceMock.Setup(e =>
+            e.SendEmailAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
             .Returns(Task.CompletedTask);
 
         var result = await _orderService.CreateOrderAsync(order);
@@ -83,8 +71,7 @@ public class OrderTests
 
         _orderRepositoryMock.Verify(r => r.AddAsync(It.IsAny<Order>()), Times.Once);
         _emailNotificationServiceMock.Verify(e =>
-            e.SendEmailAsync(order.CustomerEmail, "Order created", "Your order has been created"),
-            Times.Once);
+            e.SendEmailAsync(order.CustomerEmail, "Order created", "Your order has been created"), Times.Once);
     }
 
     [Test]
@@ -94,12 +81,11 @@ public class OrderTests
             CustomerEmail = "craig@untiedshoes.co.uk" 
         };
 
-        _orderRepositoryMock
-            .Setup(o => o.AddAsync(It.IsAny<Order>()))
-            .Returns(Task.CompletedTask);
+        _orderRepositoryMock.Setup(o => o.AddAsync(It.IsAny<Order>()))
+                            .Returns(Task.CompletedTask);
 
-        _emailNotificationServiceMock
-            .Setup(e => e.SendEmailAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+        _emailNotificationServiceMock.Setup(e =>
+            e.SendEmailAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
             .ThrowsAsync(new Exception("Email failed"));
 
         var result = await _orderService.CreateOrderAsync(order);
@@ -115,41 +101,9 @@ public class OrderTests
             CustomerEmail = "craig@untiedshoes.co.uk" 
         };
 
-        _orderRepositoryMock
-            .Setup(o => o.AddAsync(It.IsAny<Order>()))
-            .ThrowsAsync(new Exception("DB error"));
+        _orderRepositoryMock.Setup(o => o.AddAsync(It.IsAny<Order>()))
+                            .ThrowsAsync(new Exception("DB error"));
 
         Assert.ThrowsAsync<Exception>(() => _orderService.CreateOrderAsync(order));
-    }
-
-    // OrdersController Tests
-  
-    [Test]
-    public async Task GetAsync_ShouldReturnOk_WhenOrderExists()
-    {
-        var order = new Order { 
-            Id = 1, 
-            CustomerEmail = "craig@untiedshoes.co.uk" 
-        };
-
-        _orderServiceMock.Setup(s => s.GetOrderAsync(1)).ReturnsAsync(order);
-
-        var result = await _ordersController.GetAsync(1);
-
-        Assert.That(result, Is.TypeOf<OkObjectResult>());
-        var okResult = result as OkObjectResult;
-        Assert.That(okResult!.Value, Is.EqualTo(order));
-        _orderServiceMock.Verify(s => s.GetOrderAsync(1), Times.Once);
-    }
-
-    [Test]
-    public async Task GetAsync_ShouldReturnNotFound_WhenOrderDoesNotExist()
-    {
-        _orderServiceMock.Setup(s => s.GetOrderAsync(42)).ReturnsAsync((Order?)null);
-
-        var result = await _ordersController.GetAsync(42);
-
-        Assert.That(result, Is.TypeOf<NotFoundResult>());
-        _orderServiceMock.Verify(s => s.GetOrderAsync(42), Times.Once);
     }
 }
